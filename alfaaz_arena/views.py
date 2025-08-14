d8302cec721b78f25501844fec479e708191b3fd
Saurabh667 <saurav000667@gmail.com>
2025-08-15 02:35:18 +0530

    django loop

--- alfaaz_arena/views.py
+++ alfaaz_arena/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from placeData.models import destinationData
 
 def home(request):
-    return render(request,'home.html')
+    destination=destinationData.objects.all()
+    destinations={
+        'destination':destination,
+    }
+    return render(request,'home.html',destinations)
 def login(request):
     return render(request,'login.html')
