eed9d0775cdc8dc9a094f35d6c2a943919720588
Saurabh667 <saurav000667@gmail.com>
2025-08-17 16:03:59 +0530

    user uploads

--- alfaaz_arena/settings.py
+++ alfaaz_arena/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'tinymce',
     'placeData',
     'signup',
+    'uploadedDatas',
 ]
 
 MIDDLEWARE = [
---
+++ uploadedDatas/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UploadeddatasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'uploadedDatas'
