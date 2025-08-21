c5cc2d4a93025f793739efdd1f1de9f8cb6acfa0
AyushGaur123 <ayushgaur914@gmail.com>
2025-08-21 23:02:36 +0530

    new uploads

--- uploadedDatas/admin.py
+++ uploadedDatas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from uploadedDatas.models import userUploads
 
 # Register your models here.
+class userUploadsDisplay(admin.ModelAdmin):
+    list_display = ('destinationName','destinationImg','destinationDesc')
+
+admin.site.register(userUploads,userUploadsDisplay)
